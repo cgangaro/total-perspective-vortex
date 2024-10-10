c238342b1d5723fc4f22aab8f600447f51b8fbf3
cgangaro <rocket4duck@hotmail.com>
2024-10-10 14:39:31 +0200

    a

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,37 +1,61 @@
 from typing import List
 import mne
-import matplotlib.pyplot as plt
 from mne.datasets import eegbci
-from errorClasses import PreprocessingError
-from mne.preprocessing import ICA
+from mne.channels import make_standard_montage
+import matplotlib
+import matplotlib.pyplot as plt
 
 class Preprocessing:
     @staticmethod
-    def initRawFiles(subjects: List[int]):
-        # real_execution = [3, 5, 7, 9, 11, 13]
-        # imaginary_execution = [4, 6, 8, 10, 12, 14]
-        real_execution = [5]
-        imaginary_execution = [6]
+    def initRawFiles(subjects: List[int], executions: List[int]):
+        real_execution = [3, 5, 7, 9, 11, 13]
+        imaginary_execution = [4, 6, 8, 10, 12, 14]
+        one_fist_execution = [3, 4, 7, 8, 11, 12]
+        both_fists_or_feet_execution = [5, 6, 9, 10, 13, 14]
+
+        all_executions = sorted(executions)
+        print("All executions: ", all_executions)
+        # real_execution = [5]
+        # imaginary_execution = [6]
         print("Initializing raw files with subjects: ", subjects)
+        real_one_event_mapping = {1: 'rest', 2: 'do/left_fist', 3: 'do/right_fist'}
+        imaginary_one_event_mapping = {1: 'rest', 2: 'imagine/left_fist', 3: 'imagine/right_fist'}
+        real_both_event_mapping = {1: 'rest', 2: 'do/hands', 3: 'do/feet'}
+        imaginary_both_event_mapping = {1: 'rest', 2: 'imagine/hands', 3: 'imagine/feet'}
         raw_files = []
         for subject in subjects:
-            for run in real_execution:
-                event_mapping = {1: 'rest', 2: 'do/feet', 3: 'do/hands'}
+            for run in all_executions:
+                if (run in real_execution):
+                    if (run in one_fist_execution):
+                        event_mapping = real_one_event_mapping
+                    else:
+                        event_mapping = real_both_event_mapping
+                else:
+                    if (run in imaginary_execution):
+                        if (run in one_fist_execution):
+                            event_mapping = imaginary_one_event_mapping
+                        else:
+                            event_mapping = imaginary_both_event_mapping
                 raw_file = Preprocessing.loadRawFile(subject, run, event_mapping)
                 print("Raw file loaded for subject ", subject, " and run ", run)
                 raw_files.append(raw_file)
-            for run in imaginary_execution:
-                event_mapping = {1: 'rest', 2: 'imagine/feet', 3: 'imagine/hands'}
-                raw_file = Preprocessing.loadRawFile(subject, run, event_mapping)
-                print("Raw file loaded for subject ", subject, " and run ", run)
-                raw_files.append(raw_file)
+            # for run in real_execution:
+            #     event_mapping = {1: 'rest', 2: 'do/feet', 3: 'do/hands'}
+            #     raw_file = Preprocessing.loadRawFile(subject, run, event_mapping)
+            #     print("Raw file loaded for subject ", subject, " and run ", run)
+            #     raw_files.append(raw_file)
+            # for run in imaginary_execution:
+            #     event_mapping = {1: 'rest', 2: 'imagine/feet', 3: 'imagine/hands'}
+            #     raw_file = Preprocessing.loadRawFile(subject, run, event_mapping)
+            #     print("Raw file loaded for subject ", subject, " and run ", run)
+            #     raw_files.append(raw_file)
         print("ok")
         print(raw_files)
         raw = mne.io.concatenate_raws(raw_files)
         print(raw)
         events, event_dict = mne.events_from_annotations(raw)
         print(event_dict)
-        return raw
+        return raw, events, event_dict
     
     @staticmethod
     def loadRawFile(subject: int, run: int, event_mapping: dict):
@@ -52,7 +76,48 @@ class Preprocessing:
         # sfreq = sampling frequency, it's used here to relate precisely the annotations to the data
         # orig_time = original time of the recording
         raw_file.set_annotations(annot_from_events)
-        return raw_file 
+        return raw_file
+    
+    @staticmethod
+    def makeMontage(raw, montageType = "standard_1005", display=False):
+        # MONTAGE
+        eegbci.standardize(raw)
+        montage = make_standard_montage(montageType)
+        raw.set_montage(montage, on_missing='ignore')
+        montage = raw.get_montage()
+        if display:
+            montage.plot()
+        return raw
+
+    @staticmethod
+    def filterRawData(raw, low_cutoff, high_cutoff, notch_freq):
+        # NOTCH FILTER
+        # Notch Filter permet de supprimer les fréquences parasites. Ici, on supprime les fréquences de 60 Hz
+        # car elles sont souvent dues à des interférences électriques. Au USA, la fréquence électrique est de 60 Hz,
+        # alors qu'en Europe, elle est de 50 Hz.
+        raw.notch_filter(notch_freq, fir_design='firwin')
+
+        # BANDPASS FILTER
+        # Un filtre passe-bande permet de ne conserver que les fréquences qui nous intéressent. Ici, on ne garde que
+        # les fréquences entre 8 et 40 Hz.
+        rawFiltered = raw.filter(l_freq=low_cutoff, h_freq=high_cutoff, fir_design='firwin', skip_by_annotation='edge')
+        return rawFiltered
+    
+    @staticmethod
+    def displayPSD(raw, titlePrefix=""):
+        # POWER SPECTRAL DENSITY
+        psd = raw.compute_psd()
+        # fig1, ax1 = plt.subplots(figsize=(10, 6))
+        # fig2, ax2 = plt.subplots(figsize=(10, 6))
+        # psd.plot(average=False, ax=ax1)
+        # ax1.set_title(titlePrefix + " - " + "Power Spectral Density")
+        # psd.plot(average=True, ax=ax2)
+        # ax2.set_title(titlePrefix + " - " + "Power Spectral Density - Moyenne")
+        fig1 = psd.plot(average=False)
+        fig1.suptitle(titlePrefix + " - Power Spectral Density")  # Ajoute un titre au graphique
+        fig2 = psd.plot(average=True)
+        fig2.suptitle(titlePrefix + " - Power Spectral Density - Moyenne")  # Ajoute un titre au graphique
+        plt.show()
 
     @staticmethod
     def visualizeRawData(raw, rawFiltered, nbChannels=5, filteringRange="8-40Hz"):
